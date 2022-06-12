86649c641e5f6dbf8a7c3d93f933dbff38b24168
EluciusFTW <guysbuss@gmail.com>
2022-06-12 22:39:18 +0200

    More validation and some cleanup

--- Program.fs
+++ Program.fs
@@ -3,28 +3,18 @@ open System
 
 let alphabet = "abcdefghijklmnopqrstuvwxyzu".ToCharArray();
 
-let numberOfLettersInWord (word: string) letter =
-    word 
-    |> Seq.countBy (fun character -> character = letter)
-    |> Seq.filter (fun pair -> fst pair)
-    |> Seq.tryHead
-    |> fun pair -> 
-        match pair with
-        | Some pair -> snd pair
-        | None -> 0
-    
-let numberOfWordsWithletter (words: seq<string>) (letter: char) =
+let countWordsContainingLetter (words: seq<string>) (letter: char) =
     words
     |> Seq.filter (fun word -> word.Contains letter)
     |> Seq.length
 
-let occurenceMap words = 
+let buildOccurenceMap words = 
     alphabet
-    |> Array.map (fun letter -> (letter, numberOfWordsWithletter words letter))
+    |> Array.map (fun letter -> (letter, countWordsContainingLetter words letter))
     |> Seq.sortBy (fun pair -> -1*(snd pair))
     |> dict
 
-let characterFilter (word: string) (state: string) position = 
+let filterAtCharacterPosition position (word: string) (state: string) = 
     let character = state[position]
     match character with
     | c when Char.IsLetter(c) && Char.IsUpper(c) -> word[position] = Char.ToLower(character)
@@ -38,11 +28,11 @@ let filterWords (words: seq<string>) (state: string) (discarded: string) =
         |> Seq.map(fun d -> word.Contains(d)) 
         |> Seq.exists(fun value -> value) 
         |> not)
-    |> Seq.filter(fun word -> characterFilter word state 0)
-    |> Seq.filter(fun word -> characterFilter word state 1)
-    |> Seq.filter(fun word -> characterFilter word state 2)
-    |> Seq.filter(fun word -> characterFilter word state 3)
-    |> Seq.filter(fun word -> characterFilter word state 4)
+    |> Seq.filter(fun word -> filterAtCharacterPosition 0 word state)
+    |> Seq.filter(fun word -> filterAtCharacterPosition 1 word state)
+    |> Seq.filter(fun word -> filterAtCharacterPosition 2 word state)
+    |> Seq.filter(fun word -> filterAtCharacterPosition 3 word state)
+    |> Seq.filter(fun word -> filterAtCharacterPosition 4 word state)
 
 let wordScore (occurenceMap: IDictionary<char, int>) word = 
     word
@@ -50,24 +40,29 @@ let wordScore (occurenceMap: IDictionary<char, int>) word =
     |> Seq.map (fun character -> occurenceMap[character])
     |> Seq.sum
 
+let args = Environment.GetCommandLineArgs();
 printfn "Welcome to Wassi, your Wordle-Assistant!"
-if Environment.GetCommandLineArgs().Length <> 3 then
-    printfn "Please check your input, it must be of the format: .\\wassi.exe <STATE> <DISCARDED>"
-    printfn "  - where <STATE> is you current information. Capital letters mean exact hits (green),"
+if (args.Length < 2 || args[1].Length <> 5 || args.Length > 3 ) then
+    printfn "Please check your input, it must be of the format: .\\wassi.exe <STATE> <DISCARDED: optional>"
+    printfn "  * where <STATE> is you current information. Capital letters mean exact hits (green),"
     printfn "    lower case letters hits at wrong positions (yellow)."
     printfn "    To indicate nothing (gray), you can use any non-letter character."
-    printfn "  - and <DISCARDED> is a list of letters you have already ruled out."
+    printfn "    The value must be exactly 5 characters long."
+    printfn "  * and <DISCARDED> is a list of letters you have already ruled out."
     exit -1
-               
-let state = Environment.GetCommandLineArgs()[1]
-let discarded = Environment.GetCommandLineArgs()[2]
-let words = System.IO.File.ReadLines("./word-lists/words.txt")
 
+let state = args[1]
+let discarded = 
+    match args.Length with
+    | 3 -> args[2]
+    | _ -> String.Empty
+
+let words = System.IO.File.ReadLines("./word-lists/words.txt") |> Seq.toArray
 let filteredWords = filterWords words state discarded
-let currentLetterCount = occurenceMap filteredWords
+let occurenceMap = buildOccurenceMap filteredWords
 
-filteredWords
-|> Seq.map (fun word -> (word, wordScore currentLetterCount word))
+filteredWords 
+|> Seq.map (fun word -> (word, wordScore occurenceMap word))
 |> Seq.sortBy (fun pair -> -1*(snd pair))
 |> Seq.truncate(20)
 |> Seq.iter (fun pair -> printfn "The word '%s' scored '%i' points" (fst pair) (snd pair))
